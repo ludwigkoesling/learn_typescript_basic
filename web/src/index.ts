b7a182f00aaf7a21783fa0063c4d43ff941a3ff9
FUM-GLOBAL\ludwig.koesling <ludwig.koesling@ve-ict.net>
2023-11-09 11:30:50 +0100

    add user collection model

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,13 +1,7 @@
 import { User } from './models/User';
 
-const user = User.buildUser({ id: 1 });
-
-user.on('change', () => {
-  console.log(user);
+const collection = User.buildUserCollection();
+collection.on('change', () => {
+  console.log(collection);
 });
-
-user.fetch();
-
-user.set({ id: 1, name: 'ludwich', age: 20 });
-
-user.save();
+collection.fetch();
