f2a3856ba823a8152079928f921689723ad5db73
FUM-GLOBAL\ludwig.koesling <ludwig.koesling@ve-ict.net>
2023-12-07 10:58:18 +0100

    add user collection

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,12 +1,17 @@
-import { User } from './models/User';
-import { UserEdit } from './views/UserEdit';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
 
-const root = document.getElementById('root');
-const user = User.buildUser({ name: 'Lude', age: 32 });
-
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-} else {
-  throw new Error('No html rendered');
-}
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+users.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+users.fetch();
